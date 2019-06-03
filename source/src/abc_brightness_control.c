8760f9804d25977e2b1e0f999971aaa988bf47ff
John <yv3r3nd5yi1k@mail.ru>
2019-06-03 18:39:24 +0100

    Fixed set brightness test

--- source/src/abc_brightness_control.c
+++ source/src/abc_brightness_control.c
@@ -4,14 +4,37 @@
 
 #include "abc_brightness_control.h"
 
+/*********************
+ * Standard includes *
+ *********************/
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <inttypes.h>
+
 /*******************************
  * Global function definitions *
  *******************************/
 
 void
-abc_setBrightness(const abc_DisplayBrightnessInfo_t *pDisplayInfo,
+abc_setBrightness(const abc_DisplayBrightnessInfo_t *const pDisplayInfo,
                   const double brightnessValue)
 {
-    (void)pDisplayInfo;
-    (void)brightnessValue;
+    // Read maximum brightness.
+    FILE *const pMaxDisplayBrightnessFile = fopen(pDisplayInfo->pMaxDisplayBrightnessPath, "r");
+
+    uint16_t maxBrightness = 0;
+
+    (void)fscanf(pMaxDisplayBrightnessFile, "%"SCNu16, &maxBrightness);
+
+    fclose(pMaxDisplayBrightnessFile);
+
+    // Set new brightness.
+    FILE *const pDisplayBrightnessFile = fopen(pDisplayInfo->pDisplayBrightnessPath, "w");
+
+    (void)fprintf(pDisplayBrightnessFile, "%"PRIu16, (uint16_t)((brightnessValue / 100.0) * maxBrightness));
+
+    fclose(pMaxDisplayBrightnessFile);
 }
