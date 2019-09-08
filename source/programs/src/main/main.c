86d83e84abf3a96c6722a00b4dcab07278766b32
John <yv3r3nd5yi1k@mail.ru>
2019-09-08 21:16:03 +0100

    programs: Removed temporary check from main

--- source/programs/src/main/main.c
+++ source/programs/src/main/main.c
@@ -2,25 +2,15 @@
 
 #include <unistd.h>
 
-#include <stdio.h>
-
-#include <config.h>
-
 int main(void)
 {
-    abc_brightnessService_setPeriod(4);
+    abc_brightnessService_setPeriod(3);
 
     abc_brightnessService_start();
 
-#ifdef ABC_TEST_123
-	printf("Good\n");
-#else
-	printf("Bad\n");
-#endif
-
     do
     {
-        sleep(4);
+        sleep(3);
     }
     while (abc_brightnessService_wakeUp() == ABC_BRIGHTNESSSERVICE_SUCCESS);
 
