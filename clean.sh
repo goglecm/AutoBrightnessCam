828c988009546881d1ce586ab1ea090d4ea77f20
John <yv3r3nd5yi1k@mail.ru>
2019-09-24 20:26:13 +0100

    build: added autotools build artifacts

---
+++ clean.sh
@@ -0,0 +1,1 @@
+rm -rf aclocal.m4 ar-lib autom4te.cache/ build/ compile config.h.in config.guess config.h.in~ config.sub configure depcomp install-sh ltmain.sh Makefile.in missing test-driver source/Makefile.in
