4559a598cf323f9cc5817cbe83be4a5a0b4eac33
John <yv3r3nd5yi1k@mail.ru>
2019-09-24 19:48:39 +0100

    main: added argv parser for help, version and usage

--- source/programs/src/main/main.c
+++ source/programs/src/main/main.c
@@ -1,18 +1,43 @@
 #include "abc_brightness_service/abc_brightness_service.h"
 
+#include <stdlib.h>
+#include <argp.h>
 #include <unistd.h>
 
-int main(void)
+#ifndef CONFIG_H_INCLUDED
+#define CONFIG_H_INCLUDED
+#include <config.h>
+#endif // #ifndef CONFIG_H_INCLUDED
+
+const char *argp_program_version     = PACKAGE_STRING;
+const char *argp_program_bug_address = "<" PACKAGE_BUGREPORT ">";
+
+static const char doc[] =
+    "App to automatically adjust the display brightness using the luminosity "
+    "information from the webcam. To start it, simply pass no arguments.";
+
+static struct argp argp = { .doc = doc };
+
+int main(int argc, char **argv)
 {
-    abc_brightnessService_setPeriod(10);
-
-    abc_brightnessService_start();
-
-    do
+    if (argc > 1)
     {
-        sleep(10);
-    }
-    while (abc_brightnessService_wakeUp() == ABC_BRIGHTNESSSERVICE_SUCCESS);
+        argp_parse(&argp, argc, argv, 0, 0, 0);
 
-    return -1;
+        exit(0);
+    }
+    else
+    {
+        abc_brightnessService_setPeriod(10);
+
+        abc_brightnessService_start();
+
+        do
+        {
+            sleep(10);
+        }
+        while (abc_brightnessService_wakeUp() == ABC_BRIGHTNESSSERVICE_SUCCESS);
+
+        return -1;
+    }
 }
