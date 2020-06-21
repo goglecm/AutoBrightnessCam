18d2907cf5fb17a502af8e181f7a8f4ccb472209
John <yv3r3nd5yi1k@mail.ru>
2020-06-21 19:18:59 +0100

    [clean]: Now removing any install directories

--- clean.sh
+++ clean.sh
@@ -1,1 +1,1 @@
-rm -rf INSTALL aclocal.m4 ar-lib autom4te.cache/ build/ compile config.h.in config.guess config.h.in~ config.sub configure depcomp install-sh ltmain.sh Makefile.in missing test-driver source/Makefile.in
+rm -rf install INSTALL aclocal.m4 ar-lib autom4te.cache/ build/ compile config.h.in config.guess config.h.in~ config.sub configure depcomp install-sh ltmain.sh Makefile.in missing test-driver source/Makefile.in
