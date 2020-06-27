361522a0897fa17ed73c32d429ab53f449513b2c
John <yv3r3nd5yi1k@mail.ru>
2020-06-27 18:54:15 +0100

    [dev tools]: Opening some more files for editing [skip ci]

--- vim_all_source.sh
+++ vim_all_source.sh
@@ -1,1 +1,1 @@
-vim configure.ac Makefile.am ./.vimrc `find source -type f \( -name "*.am" -o -name "*.c" -o -name "*.h" -o -name "*.cpp" \)`
+vim source/data/config/aubrca/defaults.conf TODO.md configure.ac Makefile.am ./.vimrc `find source -type f \( -name "*.am" -o -name "*.c" -o -name "*.h" -o -name "*.cpp" \)`
