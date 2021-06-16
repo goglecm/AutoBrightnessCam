28cae674fc953d2dbcc011251616dbfa42021f26
John <yv3r3nd5yi1k@mail.ru>
2021-06-16 14:59:43 +0100

    [vim]: Added more sources during startup

--- vim_all_source.sh
+++ vim_all_source.sh
@@ -1,1 +1,1 @@
-vim ./vim_all_source.sh ./.travis.yml source/data/config/aubrca/defaults.conf TODO.md configure.ac Makefile.am ./.vimrc `find source -type f \( -name "*.am" -o -name "*.c" -o -name "*.h" -o -name "*.cpp" \)`
+vim ./ChangeLog ./vim_all_source.sh ./SPECS/*.spec ./.travis.yml source/data/config/aubrca/defaults.conf TODO.md configure.ac Makefile.am ./.vimrc `find source -type f \( -name "*.am" -o -name "*.c" -o -name "*.h" -o -name "*.cpp" \)`
