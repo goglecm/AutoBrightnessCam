f8e015bb5a31450e984530c39f83f25be6706e14
John <yv3r3nd5yi1k@mail.ru>
2021-07-04 21:16:01 +0100

    [vim]: Added the mk files to the list of startup files

--- vim_all_source.sh
+++ vim_all_source.sh
@@ -1,1 +1,1 @@
-vim ./ChangeLog ./vim_all_source.sh ./SPECS/*.spec ./.travis.yml source/data/config/aubrca/defaults.conf TODO.md configure.ac Makefile.am ./.vimrc `find source -type f \( -name "*.am" -o -name "*.c" -o -name "*.h" -o -name "*.cpp" \)` README.md
+vim ./ChangeLog ./vim_all_source.sh ./SPECS/*.spec ./.travis.yml source/data/config/aubrca/defaults.conf TODO.md configure.ac Makefile.am ./.vimrc `find source -type f \( -name "*.mk" -o -name "*.am" -o -name "*.c" -o -name "*.h" -o -name "*.cpp" \)` README.md
